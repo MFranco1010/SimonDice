cdd55c3dff2960d368f438bdca69b94b85374ec1
MFranco1010 <88505051+MFranco1010@users.noreply.github.com>
2023-10-30 16:50:52 -0300

    update

--- src/components/Cont.js
+++ src/components/Cont.js
@@ -1,11 +1,27 @@
 import React from 'react'
+import { useState } from 'react'
 
 export const Cont = () => {
+  const [points, setPoints] = useState(0)
+  const [jug, setJugador] = useState("ejemplo")
+
+  const plus = () => {
+    setPoints(points + 1);
+  };
+
+  const name = (a) => {
+    setJugador(a);
+  };
   return (
     <div className='cont'>
-        <p className='Wel'>Bienvenido Jugador</p>
+        <div className='info'>
+          <p className='welcome'>Bienvenido {jug} </p>
+          <input className='input' placeholder='Ingresa tu nombre' onSubmit={name} />
+          <button className='inputbtn' type='submit'>👍</button>
+          <p className='points'>Puntuacion: { points } </p>
+          </div>
         <div className='btns'>
-            <button className='red'></button>
+            <button className='red' onClick={plus} ></button>
             <button className='yel'></button>
             <button className='gre'></button>
             <button className='pin'></button>
