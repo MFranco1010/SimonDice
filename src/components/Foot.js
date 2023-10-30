1ca9cf2c88d808bda4582e1510f41c12570e4a47
MFranco1010 <88505051+MFranco1010@users.noreply.github.com>
2023-10-30 12:12:03 -0300

    update

---
+++ src/components/Foot.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export const Foot = () => {
+  return (
+    <>
+    <div>
+        <p>Franco David Machado 2023</p>
+    </div>
+    <div>
+        <ul>
+            <li>Github</li>
+            <li>Vercel</li>
+            <li>Onlyfans</li>
+        </ul>
+    </div>
+    </>
+  )
+}
---
+++ src/components/Nav.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Nav = () => {
+  return (
+    <div className='nav'>
+        <div className='logo'>LOGO</div>
+        <div className='title'>SIMON DICE!</div>
+    </div>
+  )
+}
