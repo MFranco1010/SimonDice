31b78b30a44420364d81816e37afccd7fcdd381d
MFranco1010 <88505051+MFranco1010@users.noreply.github.com>
2023-11-06 12:29:18 -0300

    update

--- src/components/Cont.js
+++ src/components/Cont.js
@@ -1,12 +1,26 @@
 import React from 'react'
 import { useState } from 'react'
-const arr = [];
+let array = [];
+let nombre = "";
 
 export const Cont = () => {
   const [points, setPoints] = useState(0);
   const [position, setPosition] = useState(0);
   const [jug, setJug] = useState("");
-
+  const [arr, setArr] = useState([]);
+  
+  const arreglo = () => {
+    if (position%10 == 0) {
+      for (let i = 0; i < 5; i++) {
+        array.push(Math.floor(Math.random(1)*4)+1);
+      }
+    }
+    setArr(array);
+    console.log("ARREGLO: ",array);
+    console.log("ESTADO: ",arr)
+  }
+  
+  console.log("form")
   const plus = (a) => {
     if (a.target.value == arr[position]) {
       setPoints(points + 1);
@@ -19,15 +33,18 @@ export const Cont = () => {
     }
   };  
 
+  const nam = (a) => {
+    nombre = a.target.value;
+  };
   const name = (a) => {
     a.preventDefault();
-    setJug(a.target.value);
+    setJug(nombre);
   };
   return (
     <div className='cont'>
         <form onSubmit={name} className='info'>
           <p className='welcome'>Bienvenido {jug} Combinacion: {arr}</p>
-          <input required className='input' maxLength={18} placeholder='Ingresa tu nombre'/>
+          <input required className='input' onChange={nam} maxLength={18} placeholder='Ingresa tu nombre'/>
           <button className='inputbtn' type='submit'>👍</button>
           <p className='points'>Puntuacion: { points } </p>
         </form>
@@ -37,7 +54,7 @@ export const Cont = () => {
             <button value={3} className='gre' onClick={plus} ></button>
             <button value={4} className='pin' onClick={plus} ></button>
         </div>
-        <button className='jgr' > JUGAR </button>
+        <button className='jgr' onClick={arreglo}><p className='jgrtext'>JUGAR</p></button>
     </div>
   )
 }
